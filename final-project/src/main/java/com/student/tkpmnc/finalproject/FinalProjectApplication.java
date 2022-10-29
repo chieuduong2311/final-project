82a28b8cd6da4393ebc1bcbd9ba8c9bf3ce48690
Duong Nguyen <chieuduong2311@hotmail.com>
2022-10-29 14:54:50 +0700

    switch to socketio server

--- final-project/src/main/java/com/student/tkpmnc/finalproject/FinalProjectApplication.java
+++ final-project/src/main/java/com/student/tkpmnc/finalproject/FinalProjectApplication.java
@@ -1,12 +1,30 @@
 package com.student.tkpmnc.finalproject;
 
+import com.corundumstudio.socketio.Configuration;
+import com.corundumstudio.socketio.SocketIOServer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableFeignClients
 public class FinalProjectApplication {
+
+	@Value("${rt-server.host}")
+	private String host;
+
+	@Value("${rt-server.port}")
+	private Integer port;
+
+	@Bean
+	public SocketIOServer socketIOServer() {
+		Configuration config = new Configuration();
+		config.setHostname(host);
+		config.setPort(port);
+		return new SocketIOServer(config);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(FinalProjectApplication.class, args);
