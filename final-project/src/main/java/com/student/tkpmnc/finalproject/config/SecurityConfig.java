c76ea57db1fb347487629b36507d7b5d8683b4d1
Duong Nguyen <chieuduong2311@hotmail.com>
2022-11-12 11:46:37 +0700

    update cors

--- final-project/src/main/java/com/student/tkpmnc/finalproject/config/SecurityConfig.java
+++ final-project/src/main/java/com/student/tkpmnc/finalproject/config/SecurityConfig.java
@@ -18,6 +18,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 
+import java.util.List;
+
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -57,7 +59,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues())
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
+        corsConfiguration.setAllowedOrigins(List.of("*"));
+        corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PUT", "OPTIONS", "PATCH", "DELETE"));
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setExposedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
+
+        http.cors().configurationSource(request -> corsConfiguration)
                 .and().csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/api/swagger-ui.html").permitAll()
